b5a0976b152cc468e2d643343bb2a84201c70675
q6112345 <q6112345@qq.com>
2015-05-16 19:07:07 +0800

    Cutomized tweeter_tree_view,add tweeter model  tweeter_ids a Many2one field

--- models.py
+++ models.py
@@ -7,9 +7,10 @@ class Tweeter(models.Model):
     _name = "ott.tweeter"
     name = fields.Char(string="Screen Name", request=True)
     description = fields.Text()
+    tweets_ids = fields.One2many('ott.tweet', 'poster_id', string="Tweets")
 
 class Tweet(models.Model):
     _name = "ott.tweet"
     content = fields.Char(string="Content")
 #    time =fields.Char(string="Time")
-    poster = fields.Many2one('ott.tweeter', ondelete="cascade", string="Poster")
+    poster_id = fields.Many2one('ott.tweeter', ondelete="cascade", string="Poster")
