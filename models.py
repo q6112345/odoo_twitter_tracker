28f9199a884dafb5412b0ba7bd56ecb1df10fb9d
q6112345 <q6112345@qq.com>
2015-05-16 17:07:34 +0800

    Creat Tweeter and Tweet two models

--- models.py
+++ models.py
@@ -2,7 +2,13 @@
 
 from openerp import models, fields, api
 
-# class odoo_twitter_tracker(models.Model):
-#     _name = 'odoo_twitter_tracker.odoo_twitter_tracker'
 
-#     name = fields.Char()
+class Tweeter(models.Model):
+    _name = "ott.tweeter"
+    name = fields.Char(string="Screen Name", request=True)
+    description = fields.Text()
+
+class Tweet(models.Model):
+    _name = "ott.tweet"
+    content = fields.Char()
+    poster = fields.Many2one('ott.tweeter', ondelete="cascade", string="Poster")
